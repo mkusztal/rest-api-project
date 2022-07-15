7903972deb22af5b1aa0b28e34fc06da570101a4
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2022-07-15 14:10:47 +0300

    Change condition in seats.routes

--- routes/seats.routes.js
+++ routes/seats.routes.js
@@ -29,7 +29,7 @@ router.route('/seats').post((req, res) => {
   };
 
   if (
-    db.seat.some(
+    !db.seat.some(
       (seat) => seat.seat == newObject.seat && seat.day == newObject.day
     )
   ) {
