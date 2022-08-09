8d72daacd5285d6b644ce96697050aaac01d5de6
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2022-08-09 16:24:08 +0300

    Server connect to mongodb

--- server.js
+++ server.js
@@ -1,19 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
 const path = require('path');
-const socket = require('socket.io');
+const mongoose = require('mongoose');
 
-const server = app.listen(process.env.PORT || 8000, () => {
-  console.log('Server is running on port: 8000');
-});
+const app = express();
 
-const io = socket(server);
-io.on('connection', (socket) => {
-  console.log('New socket!' + socket.id);
-});
-
-//import endpoints
 const testimonials = require('./routes/testimonials.routes');
 const concerts = require('./routes/concerts.routes');
 const seats = require('./routes/seats.routes');
@@ -24,14 +15,29 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/client/build')));
 
-app.use((req, res, next) => {
-  req.io = io;
-  next();
-});
-
 app.use('/api', testimonials);
 app.use('/api', concerts);
 app.use('/api', seats);
+
+mongoose.connect('mongodb://localhost:27017/NewWaveDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('Connected to the database');
+});
+
+db.on('error', (err) => {
+  console.log('Error: ', err);
+});
+
+app.use((req, res, next) => {
+  req.db = db;
+  next();
+});
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build/index.html'));
@@ -39,4 +45,8 @@ app.get('*', (req, res) => {
 
 app.use((req, res) => {
   res.status(404).send('404 not found...');
+});
+
+const server = app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running on port: 8000');
 });
